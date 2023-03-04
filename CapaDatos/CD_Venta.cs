5c41af2c7c0b60ab21f493fab4a221d586af5df1
Hans Davila <hansricardodavila@outlook.com>
2023-03-03 21:35:19 -0600

    Metodos de Realizar pago desarrollados

---
+++ CapaDatos/CD_Venta.cs
@@ -0,0 +1,60 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CD_Venta
+    {
+        public bool Registrar(Venta obj, DataTable DetalleVenta ,out string Mensaje)
+        {
+            //Variable que nos indica si el usuario fue actualizado
+            bool respuesta = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    //Se crea comando con store procedure
+                    SqlCommand cmd = new SqlCommand("usp_RegistrarVenta", oconexion);
+                    cmd.Parameters.AddWithValue("IdCliente", obj.IdCliente);
+                    cmd.Parameters.AddWithValue("TotalProducto", obj.TotalProducto);
+                    cmd.Parameters.AddWithValue("MontoTotal", obj.MontoTotal);
+                    cmd.Parameters.AddWithValue("Contacto", obj.Contacto);
+                    cmd.Parameters.AddWithValue("IdDistrito", obj.IdDistrito);
+                    cmd.Parameters.AddWithValue("Telefono", obj.Telefono);
+                    cmd.Parameters.AddWithValue("Direccion", obj.Direccion);
+                    cmd.Parameters.AddWithValue("IdTransaccion", obj.IdTransaccion);
+                    cmd.Parameters.AddWithValue("DetalleVenta", DetalleVenta);
+                    
+                    cmd.Parameters.AddWithValue("Resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oconexion.Open();
+
+                    //Se ejecuta el store procedure que actualiza el campo del Id con la informacion pasada
+                    cmd.ExecuteNonQuery();
+
+                    //Se obtiene el 1 (osea true) del store procedure
+                    respuesta = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                respuesta = false;
+                Mensaje = ex.Message;
+            }
+
+            return respuesta;
+
+        }
+    }
+}
